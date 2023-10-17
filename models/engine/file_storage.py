9a2dede475a64b46066d089eb3dfe8533f02081a
milkawinoh <milkawinoh@gmail.com>
2023-10-17 14:57:09 +0300

    file json updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,12 +22,8 @@ class FileStorage:
         __objects (dict): A dictionary to store all objects, with the keys as "<class name>.id".
     """
 
-    def __init__(self):
-        """
-        Initializes a new FileStorage instance.
-        """
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """
